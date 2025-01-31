83d1fe8209fbad2297c3d773e695f4fd0ddfc897
minasi-luca <minasi.luca@iisgalvanimi.edu.it>
2025-01-31 10:50:55 +0000

    tutto finito e funzionante

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from spotipy.oauth2 import SpotifyOAuth
 
 SPOTIFY_CLIENT_ID = "3a9130b1a74740e4a1ef72b187a3d597"
 SPOTIFY_CLIENT_SECRET = "42089d774c5d4b3a9896fa6ff7e94ab1"
-SPOTIFY_REDIRECT_URI = "http://127.0.0.1:5000/callback" #dopo il login andiamo qui
+SPOTIFY_REDIRECT_URI = "https://5000-minasiluca-spotifyminas-y1cwhxbd3yh.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
 
 app = Flask(__name__)
 app.secret_key = 'chiave_per_session' #ci serve per identificare la sessione
@@ -14,7 +14,8 @@ sp_oauth = SpotifyOAuth(
     client_id=SPOTIFY_CLIENT_ID,
     client_secret=SPOTIFY_CLIENT_SECRET,
     redirect_uri=SPOTIFY_REDIRECT_URI,
-    scope="user-read-private" #permessi x informazioni dell'utente
+    scope="user-read-private", #permessi x informazioni dell'utente
+    show_dialog=True #forziamo la richiesta di inserire new credenziali
 )
 
 @app.route('/')
@@ -37,4 +38,33 @@ def home():
     sp = spotipy.Spotify(auth=token_info['access_token']) #usiamo il token per ottenere i dati del profilo
     user_info = sp.current_user()
     print(user_info) #capiamo la struttura di user_info per usarle nel frontend
-    return render_template('home.html', user_info=user_info) #passo le info utente all'home.html
+    playlists = sp.current_user_playlists() #sempre tramite il token sp preso prima
+    playlists_info = playlists['items']
+    return render_template('home.html', user_info=user_info, playlists = playlists_info) #passo le info utente all'home.html
+    
+@app.route('/logout')
+def logout():
+    session.clear() #cancelliamo l'access token salvato in session
+    return redirect(url_for('login'))
+
+
+@app.route('/playlist/<playlist_id>')
+def view_songs(playlist_id):
+    token_info = session.get('token_info', None)
+    if not token_info:
+        return redirect(url_for('login'))
+
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    
+    playlist = sp.playlist(playlist_id)
+    
+    results = sp.playlist_items(playlist_id)
+    tracks = results['items']  # Lista di canzoni
+    
+    # Pagina di ritorno
+    return render_template('playlist.html', playlist=playlist, tracks=tracks)
+
+
+app.run(debug=True)
+
+
